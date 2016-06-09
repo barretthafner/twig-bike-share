1a831dc09e33f7109abd302cf0e2e17e39350f2a
Barrett Hafner <barrett@hafnerindustries.com>
2016-06-09 14:11:57 -0700

    remove mailgun test function

--- modules/mailgun/index.js
+++ modules/mailgun/index.js
@@ -5,26 +5,6 @@ var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 var mailer = {};
 
-mailer.sendTest = function() {
-  var params = {
-    from: "Barrett Hafner <barretth@gmail.com>",
-    to: "Barrett Hafner <barrett@hafnerindustries.com>",
-    subject: "Test Plain Mailgum email w/ HTML",
-    text: "This is the body text",
-    html: "<p> This is the html text </p>"
-  };
-
-
-
-  mailgun.messages().send(params, function (err, body) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(body);
-    }
-  });
-};
-
 mailer.sendOne = function(params, callback) {
 
   // var params = {
