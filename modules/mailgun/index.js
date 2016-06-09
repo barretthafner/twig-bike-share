3110ecaaf74c01da8928d18643746e9702584b54
Barrett Hafner <barrett@hafnerindustries.com>
2016-06-09 10:20:53 -0700

    add mailcomposer for MIME messages

--- modules/mailgun/index.js
+++ modules/mailgun/index.js
@@ -1,6 +1,8 @@
 var api_key = process.env.MAILGUN_APIKEY;
 var domain = process.env.MAILGUN_DOMAIN;
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+var mailcomposer = require('mailcomposer');
+
 
 var mailer = {};
 
@@ -19,6 +21,31 @@ mailer.sendTest = function() {
       console.log("mail sent!");
     }
   });
+
+
+  var mail = mailcomposer({
+    from: 'you@samples.mailgun.org',
+    to: 'mm@samples.mailgun.org',
+    subject: 'Test email subject',
+    body: 'Test email text',
+    html: '<b> Test email text </b>'
+  });
+
+  mail.build(function(mailBuildError, message) {
+
+    var dataToSend = {
+      to: 'mm@samples.mailgun.org',
+      message: message.toString('ascii')
+    };
+
+    mailgun.messages().sendMime(dataToSend, function (sendError, body) {
+      if (sendError) {
+        console.log(sendError);
+        return;
+      }
+    });
+  });
+
 
 
 };
