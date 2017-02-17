0b900cc0e18b44ad24f1da13714cf347ea7494b9
Barrett Hafner <barrett@hafnerindustries.com>
2017-02-17 11:25:22 -0800

    update config configuration

--- config.js
+++ config.js
@@ -11,16 +11,17 @@ config.port = process.env.PORT;
 config.dbUrl = process.env.OPENBIKEDBURL;
 config.appSecret = process.env.OPENBIKESECRET;
 
+config.appDomain = process.env.APP_DOMAIN;
 
 config.twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
 config.twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
 config.twilioSendingNumber = process.env.TWILIO_NUMBER;
-config.twilioValidateUrl = 'https://open-bike-project.herokuapp.com/api/messaging/incoming';
+config.twilioValidateUrl = 'https://' + config.appDomain + '/api/messaging/incoming';
 
 
 config.mailgunApiKey = process.env.MAILGUN_APIKEY;
-config.mailgunDomain = 'openbike.hafnerindustries.com';
-config.mailgunFromEmail = 'NoReply <noreply@openbike.hafnerindustries.com>';
+config.mailgunDomain = config.appDomain;
+config.mailgunFromEmail = 'NoReply <noreply@' + config.appDomain + '>';
 
 config.inviteSubject = '"Welcome to the Open Bike Project"';
 
