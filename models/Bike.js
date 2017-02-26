0c205726c1c3cc2c09f87c10fcd5c18a84c7ad30
Barrett Hafner <barrett@hafnerindustries.com>
2017-02-25 18:25:01 -0800

    fix bike code regex

--- models/Bike.js
+++ models/Bike.js
@@ -7,26 +7,26 @@ var BikeSchema = new mongoose.Schema({
 	bikeId: {
 		type: Number,
 		required: [true, 'Bike ID required'],
-		min: [10, 'Bike ID must be two digits'],
-		max: [99, 'Bike ID must be two digits'],
+		min: [10, 'Bike ID must be between 10 and 99'],
+		max: [99, 'Bike ID must be between 10 and 99'],
 		unique: [true, 'Bike ID must be unique']
 	},
 	code: {
 		type: String,
 		required: [true, 'Bike code is required'],
-		match: [/\d{4}/, 'Code must be a 4-digit number.']
+		match: [/^\d{4}$/, 'Code must be a 4-digit number.']
 	}
 });
 
 // findByBikeID
 // creates a query by bikeId, and returns a callback function with a potentially-null single document
 // bikeId's are unique, this should return only one result
-BikeSchema.statics.findByBikeId = function(bikeId, callback) {
+BikeSchema.statics.findByBikeId = function(bikeId, callback, errback) {
 	return this.findOne({
 		'bikeId': bikeId
 	}, function(err, bike) {
 		if (err) {
-			console.log(err);
+			errback(err);
 		} else {
 			callback(bike);
 		}
